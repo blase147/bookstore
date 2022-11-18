82032f9f261d525ec89890a8f9dd77b73e617dfa
blase147 <solarmails2@gmail.com>
2022-11-18 16:51:33 +0100

    Add components files, pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import './App.css';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Books from './pages/Books';
+import Category from './pages/Category';
 
 function App() {
   return (
-    <div className="App" />
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Books />} />
+        <Route path="/category" element={<Category />} />
+      </Routes>
+    </div>
   );
 }
 
