79afa59ca8e56db0e539cf9facc9692b5de46b23
blase147 <solarmails2@gmail.com>
2022-11-23 11:17:44 +0100

    Fix linters

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
-import store from './redux/configureStore';
 import { BrowserRouter as Router } from 'react-router-dom';
+import store from './redux/configureStore';
 import App from './App';
 
 // import css file here
@@ -12,9 +12,9 @@ import './css/index.css';
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-    <Router>
-      <App />
-    </Router>
+      <Router>
+        <App />
+      </Router>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
